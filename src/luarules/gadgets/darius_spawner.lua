4e7218583897a6356157e261a2cbc2d19b463166
jammeri <jammeri@75ba1166-da35-f791-38ae-504c9dfd57ba>
2010-06-08 11:08:25 +0000

    First attempt at spawner

---
+++ src/luarules/gadgets/darius_spawner.lua
@@ -0,0 +1,34 @@
+function gadget:GetInfo()
+	return {
+		name      = "Simple monster spawner",
+		desc      = "Spawns monster at certain rate",
+		author    = "Jammer",
+		date      = "June 2010",
+		license   = "GNU GPL, v2 or later",
+		layer     = 0,
+		enabled   = true  --  loaded by default?
+	}
+end
+
+local spEcho = Spring.Echo
+local spCreateUnit = Spring.CreateUnit
+
+if (not gadgetHandler:IsSyncedCode()) then
+	return false -- no unsynced code
+end
+
+local monsters = {
+	{"corcom",150,20},
+	{"armcom",300,20}
+}
+
+function gadget:GameFrame(f)
+	if f%50 == 0 then
+		for monster in monsters do
+			if f%monster[2] == 0 && monster[3] > 0 then
+				monster[3]--
+				spCreateUnit(monster[1],x,y,z,"south",team,false)
+			end
+		end
+	end
+end

8092779d84578934b59ca795983bb4f438938fe7
jammeri <jammeri@75ba1166-da35-f791-38ae-504c9dfd57ba>
2010-06-17 01:50:33 +0000

    Spawner's Spring commands replaced with local versions for performance

--- src/luarules/gadgets/darius_spawner.lua
+++ src/luarules/gadgets/darius_spawner.lua
@@ -12,6 +12,9 @@ end
 
 local spEcho = Spring.Echo
 local spCreateUnit = Spring.CreateUnit
+local spGetTeamUnits = Spring.GetTeamUnits
+local spGetTeamStartPosition = Spring.GetTeamStartPosition
+local spGiveOrderToUnit = Spring.GiveOrderToUnit
 
 local emptyTable = {}
 
@@ -38,14 +41,14 @@ end
 function gadget:GameFrame(f)
 	if f%50 == 0 then
 		if x == nil then
-			x,y,z = Spring.GetTeamStartPosition(0)
-			x2,y2,z2 = Spring.GetTeamStartPosition(1)
+			x,y,z = spGetTeamStartPosition(0)
+			x2,y2,z2 = spGetTeamStartPosition(1)
 		end
 		for i, monster in pairs(monsters) do
 			if (f%monster[2] == 0 and monster[3] > 0) then
 				monster[3] = monster[3] - 1
 				local unit = spCreateUnit(monster[1],x+i*200,y,z,"south",0,false)
-				Spring.GiveOrderToUnit(unit,CMD.MOVE,{x2,y2,z2},emptyTable)
+				spGiveOrderToUnit(unit,CMD.MOVE,{x2,y2,z2},emptyTable)
 			end
 		end
 	end

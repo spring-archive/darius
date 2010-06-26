2bd9b01ea4b380b2aade7e2258dc042c0e513671
jttimone <jttimone@cs.helsinki.fi>
2010-06-26 23:25:17 +0000

    Maps updated. Terrain skin should now work. Missing: terrain heights, spawn locations.

---
+++ tools/map/springmapedit_913/s32.bat
@@ -0,0 +1,2 @@
+java.exe -classpath springmapedit.jar;lib_win32/gluegen-rt.jar;lib_win32/jogl.jar;lib_win32/swt.jar -Xms128m -Xmx1024m -Djava.library.path=lib_win32 application.SpringMapEditApplication
+pause

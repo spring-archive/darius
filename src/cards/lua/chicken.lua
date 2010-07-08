a02b6cc23fe87d6e86024e94ff46972b7bea0603
kap_89 <kap_89@hotmail.com>
2010-07-08 14:31:01 +0000

    -fixed some typos and made some aesthetic changes

--- src/cards/lua/chicken.lua
+++ src/cards/lua/chicken.lua
@@ -1,15 +1,15 @@
 local card = {
-	name = "Chicken",
-	type = "Material",
-	img = 'cards/images/material/chicken.png',
-	health = 1600,
-	reloadTIme = 0.4,
-	range = 0,
-	damage = 0, 
-	greenballs = -2,
-	effect = nil,
-	desc = "Chicken bones are harder and stronger then most known metals, \n" ..
-		"but the process of turning raw bones into material is costly.",
+	name 		= "Chicken",
+	type 		= "Material",
+	img 		= 'cards/images/material/chicken.png',
+	health 		= 1600,
+	reloadTime 	= 0.4,
+	range 		= 0,
+	damage 		= 0, 
+	greenballs 	= -2,
+	effect 		= nil,
+	desc 		= "Chicken bones are harder and stronger then most known metals, \n" ..
+				  "but the process of turning raw bones into material is costly.",
 }
 
 return card
--- src/cards/lua/seises_special.lua
+++ src/cards/lua/seises_special.lua
@@ -1,15 +1,15 @@
 local card = {
-	name = "Seise's special",
-	type = "Material",
-	img = 'LuaUI/images/seises_special.png',
-	health = 10000,
-	reloadTIme = 0,
-	range = 100,
-	damage = 0,
-	greenballs = -15,
-	effect = nil,
-	desc = "Ultimate material. Made by puting a bunch of hard materials and bananas into a \n" ..
-		   "blender, throwing the blender into the sun and scooping up the remains.",
+	name 		= "Seise's special",
+	type 		= "Material",
+	img 		= 'LuaUI/images/seises_special.png',
+	health 		= 10000,
+	reloadTime 	= 0,
+	range 		= 100,
+	damage 		= 0,
+	greenballs 	= -15,
+	effect 		= nil,
+	desc 		= "Ultimate material. Made by puting a bunch of hard materials and bananas into a \n" ..
+				  "blender, throwing the blender into the sun and scooping up the remains.",
 }
 
 return card

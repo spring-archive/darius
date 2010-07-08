5b7d8e90995d7e0a77690b3d3f3de35273f333aa
anssi.syrjasalo <anssi.syrjasalo@75ba1166-da35-f791-38ae-504c9dfd57ba>
2010-07-08 20:35:33 +0000

    arranged cards into corresponding subfolders by their type

---
+++ src/cards/lua/material/stone.lua
@@ -0,0 +1,15 @@
+local card = {
+    name       = "Stone",
+    type       = "Material",
+    img        = 'cards/images/material/stone.png',
+    health     = 1200,
+    reloadTime = 0.5,
+    range      = 50,
+    damage     = 0,
+    greenballs = 0,
+    effect     = nil,
+    desc       = "Creates tall stone towers with decent range and good amount of health,\n" ..
+                 "but due to tall design, adds additional weapon reloading time."
+}
+
+return card
--- src/luarules/gadgets/darius_card_pool.lua
+++ src/luarules/gadgets/darius_card_pool.lua
@@ -21,6 +21,18 @@ local spEcho = Spring.Echo
 
 
 ---------------------
+-- Local Variables --
+---------------------
+
+local allPossibleCards = {
+	material = {},
+	weapon = {},
+	special = {}
+}
+
+
+
+---------------------
 -- Table Functions --
 ---------------------
 
@@ -47,44 +59,60 @@ if (not gadgetHandler:IsSyncedCode()) then
 end
 
 
-local function LoadCardFromFile(cardName)
-	cardName = string.lower(cardName) -- filename is in lowercase
-	cardName = string.gsub(cardName, " ", "_") -- filename has underscores instead of spaces
+local function LoadCardsFromFiles()
+	local materialFiles = VFS.DirList('cards/lua/material', '*.lua')
+	local weaponFiles = VFS.DirList('cards/lua/weapon', '*.lua')
+	local specialFiles = VFS.DirList('cards/lua/special', '*.lua')
 	
-	local cardData = {}
-	cardData = VFS.Include("cards/lua/"..cardName..".lua")
-	return cardData
+	for i=1, #materialFiles do
+		allPossibleCards.material[i] = VFS.Include(materialFiles[i])
+	end
+	
+	for i=1, #weaponFiles do
+		allPossibleCards.weapon[i] = VFS.Include(weaponFiles[i])
+	end
+	
+	for i=1, #specialFiles do
+		allPossibleCards.special[i] = VFS.Include(specialFiles[i])
+	end
+end
+
+
+local function GetCardByTypeAndName(cardType, cardName)
+	for i=1, #allPossibleCards[cardType] do
+		if allPossibleCards[cardType][i].name == cardName then
+			return allPossibleCards[cardType][i]
+		end
+	end
+	return nil
 end
 
 
 local function SendDecks()  -- Sends the decks to the instance game manager
+	for i = 1, 2 do
+		Darius:AddCard(GetCardByTypeAndName("material", "Stone"), 1)
+	end
 	
 	for i = 1, 2 do
-		Darius:AddCard(table.copy(LoadCardFromFile("Stone")), 1)
+		Darius:AddCard(GetCardByTypeAndName("material", "Metal"), 1)
+	end
+	
+	for i = 1, 2 do
+		Darius:AddCard(GetCardByTypeAndName("weapon", "Fire"), 2)
+	end
+	
+	for i = 1, 2 do
+		Darius:AddCard(GetCardByTypeAndName("weapon", "Lightning"), 2)
 	end
 
-	for i = 1, 2 do
-		Darius:AddCard(table.copy(LoadCardFromFile("Metal")), 1)
-	end
-	
-	for i = 1, 2 do
-		Darius:AddCard(table.copy(LoadCardFromFile("Fire")), 2)
-	end
-	
-	for i = 1, 2 do
-		Darius:AddCard(table.copy(LoadCardFromFile("Lightning")), 2)
-	end
-
-    -- just for testing
-	Darius:AddCard(table.copy(LoadCardFromFile("Castle Revival")), 1)
-    Darius:AddCard(table.copy(LoadCardFromFile("Earthquake")), 1)
-    Darius:AddCard(table.copy(LoadCardFromFile("Quad Damage")), 1)
-    Darius:AddCard(table.copy(LoadCardFromFile("Sandstorm")), 1)
-    Darius:AddCard(table.copy(LoadCardFromFile("Solar Radiation")), 2)
-    Darius:AddCard(table.copy(LoadCardFromFile("Temporary Degreelessness")), 2)
-    Darius:AddCard(table.copy(LoadCardFromFile("Tower Invisibility")), 2)
-    Darius:AddCard(table.copy(LoadCardFromFile("Tower Regeneration")), 2)
-    
+	Darius:AddCard(GetCardByTypeAndName("special", "Castle Revival"), 1)
+    Darius:AddCard(GetCardByTypeAndName("special", "Earthquake"), 1)
+    Darius:AddCard(GetCardByTypeAndName("special", "Quad Damage"), 1)
+    Darius:AddCard(GetCardByTypeAndName("special", "Sandstorm"), 1)
+    Darius:AddCard(GetCardByTypeAndName("special", "Solar Radiation"), 2)
+    Darius:AddCard(GetCardByTypeAndName("special", "Temporary Degreelessness"), 2)
+    Darius:AddCard(GetCardByTypeAndName("special", "Tower Invisibility"), 2)
+    Darius:AddCard(GetCardByTypeAndName("special", "Tower Regeneration"), 2)
 end
 
 
@@ -96,7 +124,10 @@ local function StartGame()
 	
 	Darius = GG.Darius
 	Darius:ClearGame()
+	
+	LoadCardsFromFiles()
 	SendDecks()
+	
 	Darius:AddGreenballs(40)
 end
 

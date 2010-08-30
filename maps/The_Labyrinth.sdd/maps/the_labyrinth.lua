34f4968c85ba472d89699042823ac4c93ae27117
peitsa82 <peitsa82@hotmail.com>
2010-08-30 20:21:03 +0000

    Changed the waves on labyrinth. The map should be doable with the two default decks loaded on a fresh system.

--- maps/The_Labyrinth.sdd/maps/the_labyrinth.lua
+++ maps/The_Labyrinth.sdd/maps/the_labyrinth.lua
@@ -1,68 +1,40 @@
 return {
 	name = "The Labyrinth",
 	waves = {
-		easy = {
+			easy = {
+			{duration = 10,
+			{ monster = "chicken", interval = 2, amount = 1, location = 1 },
+			},
+			},
+--[[		easy = {
 			{ -- wave 1
 				duration = 30,
-				{ monster = "chicken", interval = 2, amount = 10, location = 1 },
+				{ monster = "chicken", interval = 2, amount = 5, location = 1 },
+				{ monster = "armpw", interval = 2, amount = 5, location = 1 },
 			},
 			{ -- wave 2
 				duration = 30,
-				{ monster = "corthud", interval = 3, amount = 3, location = 1 },
+				{ monster = "corthud", interval = 2, amount = 10, location = 1 },
 			},
 			{ -- wave 3
 				duration = 30,
-				{ monster = "armpw", interval = 3, amount = 5, location = 1 },
+				{ monster = "armpw", interval = 2, amount = 10, location = 1 },
 			},
 			{ -- wave 4
 				duration = 30,
-				{ monster = "arm_venom", interval = 3, amount = 3, location = 1 },
-			},
-			{ -- wave 5
-				duration = 30,
-				{ monster = "corstorm", interval = 4, amount = 3, location = 1 },
-			},
-			{ -- wave 6
-				duration = 30,
-				{ monster = "corpyro", interval = 5, amount = 3, location = 1 },
-			},
-			{ -- wave 7
-				duration = 30,
-				{ monster = "armsptk", interval = 3, amount = 3, location = 1 },
-			},
-			{ -- wave 8
-				duration = 30,
-				{ monster = "chickena", interval = 3, amount = 3, location = 1 },
-			},
-			{ -- wave 9
-				duration = 30,
-				{ monster = "chicken_dodo", interval = 5, amount = 4, location = 1 },
+				{ monster = "arm_venom", interval = 2, amount = 5, location = 1 },
+				{ monster = "corthud", interval = 2, amount = 10, location = 1 },
 			},
 			{ -- wave 10
 				duration = 30,
-				{ monster = "chicken_sporeshooter", interval = 5, amount = 4, location = 1 },
+				{ monster = "chicken_sporeshooter", interval = 2, amount = 10, location = 1 },
 			},
 			{ -- wave 11
 				duration = 30,
-				{ monster = "cormortgold", interval = 5, amount = 2, location = 1 },
+				{ monster = "cormortgold", interval = 2, amount = 2, location = 1 },
+				{ monster = "arm_venom", interval = 2, amount = 2, location = 1 },
 			},
-			{ -- wave 12
-				duration = 30,
-				{ monster = "armwar", interval = 3, amount = 3, location = 1 },
-			},
-			{ -- wave 13
-				duration = 30,
-				{ monster = "chickenc", interval = 5, amount = 2, location = 1 },
-			},
-			{ -- wave 14
-				duration = 30,
-				{ monster = "armorco", interval = 5, amount = 2, location = 1 },
-			},
-			{ -- wave 15
-				duration = 30,
-				{ monster = "chickenq", interval = 5, amount = 2, location = 1 },
-			}
-		},
+		},--]]
 		normal = {
 		},
 		hard = {
